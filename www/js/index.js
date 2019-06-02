920b382155934d324973804133d4a57786d10d05
kevinDalek <44786589+kevinDalek@users.noreply.github.com>
2019-06-03 00:28:13 +0400

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -571,7 +571,7 @@ function showSlides1() {
  
 /*************AI************************/
  /****API PART**************/
-  var accessToken = "092f1bbb4718446aa379920703e8b766",
+var accessToken = "092f1bbb4718446aa379920703e8b766",
       baseUrl = "https://api.api.ai/v1/",
       $speechInput,
       $recBtn,
@@ -580,72 +580,80 @@ function showSlides1() {
       messageCouldntHear = "I couldn't hear you, could you say that again?",
       messageInternalError = "Oh no, there has been an internal server error",
       messageSorry = "I'm sorry, I don't have the answer to that yet.";
-  /*******END OF API Part***********/
- $speechInput = $("#speech");
-      $recBtn = $("#rec");
-     /* $speechInput.keypress(function(event) {
-        if (event.which == 13) {
-          event.preventDefault();
-          send();
-        }
-      });*/
-      $('#speech').on( "change", function(event) {
 
-            send();
-         });
-      $recBtn.on("click", function(event) {
-        switchRecognition();
-      });
-    
-    
-    function startRecognition() {
-      recognition = new webkitSpeechRecognition();
-      recognition.continuous = false;
-          recognition.interimResults = false;
-      recognition.onstart = function(event) {
-        respond(messageRecording);
-        updateRec();
-      };
-      recognition.onresult = function(event) {
-        recognition.onend = null;
-        
-        var text = "";
-          for (var i = event.resultIndex; i < event.results.length; ++i) {
-            text += event.results[i][0].transcript;
-          }
-          setInput(text);
-        stopRecognition();
-      };
-      recognition.onend = function() {
-        respond(messageCouldntHear);
-        stopRecognition();
-      };
-      recognition.lang = "en-US";
-      recognition.start();
-    }
+$(document).ready(function() {
+
+  $speechInput = $("#speech");
+  $recBtn = $("#rec");
   
-    function stopRecognition() {
-      if (recognition) {
-        recognition.stop();
-        recognition = null;
-      }
-      updateRec();
-    }
-    function switchRecognition() {
-      if (recognition) {
-        stopRecognition();
-      } else {
-        startRecognition();
-      }
-    }
-    function setInput(text) {
-      $speechInput.val(text);
+  $speechInput.keypress(function(event) {
+    if (event.which == 13) {
+      event.preventDefault();
       send();
     }
-    function updateRec() {
-      $recBtn.text(recognition ? "Stop" : "Speak");
+  });
+
+  $recBtn.on("click", function(event) {
+    switchRecognition();
+  });
+  
+  $(".debug__btn").on("click", function() {
+    $(this).next().toggleClass("is-active");
+    return false;
+  });  
+});
+
+function switchRecognition() {
+  if (recognition) {
+    stopRecognition();
+  } else {
+    startRecognition();
+  }
+}
+
+function startRecognition() {
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = false;
+  recognition.interimResults = false;
+  recognition.onstart = function(event) {
+    respond(messageRecording);
+    updateRec();
+  };
+  recognition.onresult = function(event) {
+    recognition.onend = null;
+
+    var text = "";
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      text += event.results[i][0].transcript;
     }
-    function send() {
+    setInput(text);
+    stopRecognition();
+  };
+  recognition.onend = function() {
+    respond(messageCouldntHear);
+    stopRecognition();
+  };
+  recognition.lang = "en-US";
+  recognition.start();
+}
+
+function stopRecognition() {
+  if (recognition) {
+    recognition.stop();
+    recognition = null;
+  }
+  updateRec();
+}
+
+function setInput(text) {
+  $speechInput.val(text);
+  send();
+}
+
+function updateRec() {
+  $recBtn.text(recognition ? "Stop" : "Speak");
+}
+   function send() {
       var text = $speechInput.val();
       $.ajax({
         type: "POST",
@@ -664,31 +672,26 @@ function showSlides1() {
         }
       });
     }
-    function prepareResponse(val) {
-      var debugJSON = JSON.stringify(val, undefined, 2),
-        spokenResponse = val.result.speech;
-      respond(spokenResponse);
-     
-    }
+function prepareResponse(val) {
+  
+  var spokenResponse = val.result.speech;
+  // actionResponse = val.result.action;
+  // respond()
+  respond(spokenResponse);
+  
+  var debugJSON = JSON.stringify(val, undefined, 2);
+  debugRespond(debugJSON); // Print JSON to Debug window
+}
+function debugRespond(val) {
+  $("#response").text(val);
+}
+function respond(val) {
+  if (val == "") {
+    val = messageSorry;
+  }
+  if (val !== messageRecording) {
    
-    function respond(val) {
-      if (val == "") {
-        val = messageSorry;
-      }
-      if (val !== messageRecording) {
-     
-        TTS.speak(val);
-      }
-      $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(val);
-    }
-
-
-    /***Test***/
-     function play() {
-   /* TTS.speak('hello, world!', function () {
-    console.log('success');
-  }, function (reason) {
-    console.log('error', reason);
-  });*/
-  TTS.speak('Hello World');
-    }
+    TTS.speak(val);
+  }
+  $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(val);
+}
